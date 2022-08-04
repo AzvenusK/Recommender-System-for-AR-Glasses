1b27156c84cc25edf4267ebf32928840c83c88d9
Anmol Kumar <31875086+AzvenusK@users.noreply.github.com>
2022-08-04 23:18:41 +0100

    Matrix Factorization Complete

--- src/MatrixFactorization/utilities.py
+++ src/MatrixFactorization/utilities.py
@@ -1,39 +1,110 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def visualize_classifier(classifier, X, y):
-    # Define the minimum and maximum values for X and Y
-    # that will be used in the mesh grid
-    min_x, max_x = X[:, 0].min() - 1.0, X[:, 0].max() + 1.0
-    min_y, max_y = X[:, 1].min() - 1.0, X[:, 1].max() + 1.0
 
-    # Define the step size to use in plotting the mesh grid 
-    mesh_step_size = 0.01
+def top(vector, maximum, k):
+    c = maximum * np.argsort(scores)[-k:] + (1 - maximum) * np.argsort(scores)[:k]
+    d = []
+    for i in np.arange(len(c)):
+        d.append(vector[c[i]])
+    return d
 
-    # Define the mesh grid of X and Y values
-    x_vals, y_vals = np.meshgrid(np.arange(min_x, max_x, mesh_step_size), np.arange(min_y, max_y, mesh_step_size))
 
-    # Run the classifier on the mesh grid
-    output = classifier.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
+def rearrange(items, ratings):
+    attribute, scores = [], []
+    ranking = np.argsort(ratings)
 
-    # Reshape the output array
-    output = output.reshape(x_vals.shape)
+    for k in np.arange(len(ranking)):
+        attribute.append(items[ranking[k]])
+        scores.append(ratings[ranking[k]])
 
-    # Create a plot
-    plt.figure()
+    return attribute, scores
 
-    # Choose a color scheme for the plot 
-    plt.pcolormesh(x_vals, y_vals, output, cmap=plt.cm.gray)
 
-    # Overlay the training points on the plot 
-    plt.scatter(X[:, 0], X[:, 1], c=y, s=75, edgecolors='black', linewidth=1, cmap=plt.cm.Paired)
+def rank_top_k(names, ratings):
+    """
+    Example:
+    a, b = np.array(['a', 'b', 'c']), np.array([6, 1, 3])
+    a, b = rearrange(a, b)
+    >>> a
+    np.array('b', 'c', 'a')
+    >>> b
+    np.array([1, 3, 6])
+    """
 
-    # Specify the boundaries of the plot
-    plt.xlim(x_vals.min(), x_vals.max())
-    plt.ylim(y_vals.min(), y_vals.max())
+    ranked_ids = np.argsort(ratings)
+    return names[ranked_ids], ratings[ranked_ids]
 
-    # Specify the ticks on the X and Y axes
-    plt.xticks((np.arange(int(X[:, 0].min() - 1), int(X[:, 0].max() + 1), 1.0)))
-    plt.yticks((np.arange(int(X[:, 1].min() - 1), int(X[:, 1].max() + 1), 1.0)))
+
+def convert(data, nb_users, nb_movies):
+    new_data = []
+    for id_users in range(1, nb_users + 1):
+        id_movies = data[:,1][data[:,0] == id_users]
+        id_ratings = data[:,2][data[:,0] == id_users]
+        ratings = np.zeros(nb_movies)
+        ratings[id_movies - 1] = id_ratings
+        new_data.append(list(ratings))
+    return new_data
+
+
+def inner_concatenation(a, b):
+    c = []
+    for k in np.arange(len(a)):
+        c.append(a[k]+b[k])
+    return c
+
+
+def transform(user_sex, user_age, user_occupation):
+
+    argument = users_occupation_name.tolist().index(user_occupation)
+    empty = np.zeros(len(users_occupation_name))
+    empty[argument] = 1
+
+    if user_sex == 'Male':
+        user_sex = 0
+    else:
+        user_sex = 1
+
+    return np.concatenate(([user_sex], [user_age], empty.T)).T.tolist()
+
+
+def descriptive(variable):
+
+    if variable.describe().dtype == 'float64':
+        stats = pd.DataFrame(np.matrix([np.round(np.mean(variable), 2), np.round(np.std(variable), 2), np.min(variable), np.percentile(variable, 25),
+        np.round(np.median(np.array(variable)), 2), np.percentile(variable, 75), np.max(variable)]))
+        stats.columns = np.array(['Mean', 'Std', 'Min', '1 qrt', 'Med', '3 qrt', 'Max'])
+    else:
+
+        stats = pd.DataFrame(np.matrix((variable.describe().freq / len(variable) * 100)))
+        stats.columns = np.array(['Proportion'])
+    return stats
+
+
+def barplot(attribute, scores, xlabel, ylabel, title, rotation):
+
+    label = np.array(attribute)[np.array(scores) != 0]
+    scores = np.array(scores)[np.array(scores) != 0]
+
+    sns.set(rc={'figure.figsize':(6,4)})
+    sns.set(font_scale = 2)
+
+    plt.bar(label, scores)
+    plt.xlabel(xlabel, fontsize=10)
+    plt.ylabel(ylabel, fontsize=10)
+    plt.xticks(label, label, fontsize=10, rotation=rotation)
+    plt.title(title)
 
     plt.show()
+
+
+def freq_analysis(names, frequency, recommendations):
+
+    freq = []
+    for k in np.arange(len(recommendations)):
+        movie_id = names.tolist().index(recommendations[k])
+        freq.append(frequency[movie_id])
+
+    return np.array(freq)

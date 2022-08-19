e163adb2cb4103adf8f9e9ea9ec9478e59dd15b1
Anmol Kumar <31875086+AzvenusK@users.noreply.github.com>
2022-08-19 03:56:36 +0100

    Update recommender.py

--- src/MatrixFactorization/implementation/recommender.py
+++ src/MatrixFactorization/implementation/recommender.py
@@ -44,4 +44,44 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 # Prepare data for online learning
 X_train_initial, y_train_initial, X_train_update, y_train_update, X_test_update, y_test_update = train_update_test_split(articles_data, frac_new_users=0.2)
 
-articles_data.head(10)
+print(articles_data.head(10))
+
+global_mean = y_train.mean()
+pred = [global_mean for _ in range(y_test.shape[0])]
+
+rmse = mean_squared_error(y_test, pred, squared = False)
+
+print(f'\nTest RMSE: {rmse:4f}')
+
+baseline_model = BaselineModel(method='sgd', n_epochs = 20, reg = 0.005, lr = 0.01, verbose=1)
+baseline_model.fit(X_train, y_train)
+
+pred = baseline_model.predict(X_test)
+rmse = mean_squared_error(y_test, pred, squared = False)
+
+print(f'\nTest RMSE: {rmse:.4f}')
+
+baseline_model.recommend(user=20221000)
+
+baseline_model = BaselineModel(method='als', n_epochs = 20, reg = 0.5, verbose=1)
+baseline_model.fit(X_train, y_train)
+
+pred = baseline_model.predict(X_test)
+rmse = mean_squared_error(y_test, pred, squared = False)
+
+print(f'\nTest RMSE: {rmse:.4f}')
+
+
+matrix_fact = KernelMF(n_epochs = 20, n_factors = 100, verbose = 1, lr = 0.001, reg = 0.005)
+matrix_fact.fit(X_train, y_train)
+
+pred = matrix_fact.predict(X_test)
+rmse = mean_squared_error(y_test, pred, squared = False)
+
+print(f'\nTest RMSE: {rmse:.4f}')
+
+user = 20222000
+items_known = X_train.query('user_id == @user')['item_id']
+recommended_items = matrix_fact.recommend(user=user, items_known=items_known)['item_id']
+print('\n Recommended Items Are: \n')
+print(recommended_items)
